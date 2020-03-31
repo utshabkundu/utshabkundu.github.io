9965bb50992bfa3defd851546011360543417eba
TanvirMahin24 <55342128+TanvirMahin24@users.noreply.github.com>
2020-03-31 12:41:08 +0600

    pics optimised adn kundu pic added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var image = document.querySelector('#parallax-img');
 new simpleParallax(image, {
 	delay:.1,
     transition: 'cubic-bezier(0,0,0,1)',
-    scale: 1.5,
+    scale: 1.6,
 });
 
 new WOW().init();
